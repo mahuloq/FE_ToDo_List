935cc4f6a37f9aeb086581bd2c8fb367aafa1ab0
Matt Tucker <arcanemahuloq@gmail.com>
2024-02-27 21:33:54 -0600

    Added login/logout, authguard and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [TodoListComponent],
+  imports: [TodoListComponent, RouterOutlet, NavbarComponent],
 })
 export class AppComponent {}
